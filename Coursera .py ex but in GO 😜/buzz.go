3766d1fc57b387eebeba1b8ad37a099f255fc22b
Guilherme FGR <guifgr@hotmail.com>
2021-04-01 18:44:32 -0300

    coursera py to Go

---
+++ Coursera .py ex but in GO 😜/buzz.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	var x int
+	var y int
+	fmt.Println("Quantos numeros você quer digitar?")
+	fmt.Scan(&x)
+	var numeros []string
+	for i := 0; i < x; i++ {
+		fmt.Printf("Digite o numero da posição %d:\t", i+1)
+		fmt.Scan(&y)
+		if y%5 == 0 {
+			numeros = append(numeros, "buzz")
+
+		} else {
+			z := fmt.Sprintf("%v", y)
+			numeros = append(numeros, z)
+		}
+	}
+	fmt.Println(numeros)
+}
